09ebee7cb5c25d7dad974a560fd7d84c46532154
Jônatas Pereira <jonataspereirarocha@gmail.com>
2023-03-07 09:21:06 -0300

    Update scripts.js

--- relogio/js/scripts.js
+++ relogio/js/scripts.js
@@ -1,3 +1,20 @@
-let hora = document.getElementsByClassName("hora")
-let min = document.getElementsByClassName("min") 
-let seg = document.getElementsByClassName("seg")
+const hora = document.getElementById("hora")
+const min = document.getElementById("min")
+const seg = document.getElementById("seg")
+
+setInterval(() => {
+    let date = new Date()
+    let horas = date.getHours()
+    let minutos = date.getMinutes()
+    let segundos = date.getSeconds()
+
+    hora.innerHTML = `${formatTime(horas)}`
+    min.innerHTML = `${formatTime(minutos)}`
+    seg.innerHTML = `${formatTime(segundos)}`
+
+}, 1000)
+
+function formatTime(time) {
+    return time < 10 ? "0" + time : time
+}
+
